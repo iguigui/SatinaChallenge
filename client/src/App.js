7f4b889ffd1e95a71a248197fcc84c2abd19397e
iguigui <andrewgreigbarr@gmail.com>
2024-07-23 13:28:29 +0200

    Client created with basic routes and layout with fetch + create player + list all players + create game

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import CreateGame from "./components/CreateGame";
+import CreatePlayer from "./components/CreatePlayer";
+import TopPlayers from "./components/TopPlayers";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/game/new" element={<CreateGame />} />
+          <Route path="/players/new" element={<CreatePlayer />} />
+          <Route path="/players" element={<TopPlayers />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
